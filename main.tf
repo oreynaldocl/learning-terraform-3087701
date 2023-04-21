1ff589f557a2a3f327d677fb4009d1d7b77adc6f
Oscar Callisaya <oscar.callisaya@nice.com>
2023-04-21 15:33:05 -0400

    Test with 3_74.x

--- main.tf
+++ main.tf
@@ -51,17 +51,17 @@ resource "aws_security_group" "aurora_sg_2" {
   vpc_id      = data.aws_vpc.selected.id
 
   ingress {
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    cidr_blocks  = ["0.0.0.0/0"]
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks  = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags   = {
